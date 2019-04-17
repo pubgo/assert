62b92511a33d23ce3c9318f67200acc7f620b202
白云辉 <baiyunhui@yuanben.org>
2019-04-17 15:06:19 +0800

    add if

--- assert_test.go
+++ assert_test.go
@@ -6,5 +6,9 @@ import (
 
 func TestName(t *testing.T) {
 	a := &Assert{}
+	a.P(IfEquals(1, 2, 34))
+	a.P(IfEquals(nil, nil))
+	a.P(IfEquals(0, ""))
+	a.P(IfEquals("", ""))
 	a.P("hello")
 }
--- func.go
+++ func.go
@@ -6,3 +6,45 @@ var P = _a.P
 var Bool = _a.Bool
 var Err = _a.Err
 var MustNotError = _a.MustNotError
+
+func If(b bool, t, f interface{}) interface{} {
+	if b {
+		return t
+	}
+	return f
+}
+
+func IfEquals(args ...interface{}) bool {
+	if len(args) == 0 {
+		return true
+	}
+
+	_t := args[0]
+	if _t == nil {
+		return false
+	}
+
+	for i := 1; i < len(args); i++ {
+		if args[i] == nil {
+			return false
+		}
+
+		if _t != args[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func IfIn(a interface{}, args ...interface{}) bool {
+	for _, arg := range args {
+		if a == arg {
+			return true
+		}
+	}
+	return false
+}
+
+func IfNotIn(a interface{}, args ...interface{}) bool {
+	return IfIn(a, args...)
+}
